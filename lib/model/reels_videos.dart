21619dfc4fc85794bec1a72892135837e61495f3
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-04-15 01:01:31 +0300

    Removed Chewie

---
+++ lib/model/reels_videos.dart
@@ -0,0 +1,12 @@
+class ReelsVideos{
+  final String videos;
+
+
+  ReelsVideos({required this.videos});
+}
+
+
+List<ReelsVideos> reelVideos=[
+  ReelsVideos(videos: 'assets/video.mp4'),
+  ReelsVideos(videos: 'assets/road.mp4'),
+];
--- lib/screens/reelspage/reels_page.dart
+++ lib/screens/reelspage/reels_page.dart
@@ -1,15 +1,17 @@
-import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_ui/constants/color_constants.dart';
 import 'package:instagram_ui/constants/text_constants.dart';
 import 'package:video_player/video_player.dart';
+
+import '../../model/reels_videos.dart';
 
 
 class ReelsPage extends StatelessWidget {
   ReelsPage({Key? key}) : super(key: key);
 
   final List<Map> reelItems = [
-    { 'video': 'assets/videos/sss.mp4'},
+    { 'video': 'assets/videos/video.mp4'},
+    { 'video': 'assets/videos/video.mov'},
     { 'video': 'assets/videos/video2.mov'},
   ];
 
@@ -37,66 +39,77 @@ class ReelsPage extends StatelessWidget {
       body: PageView.builder(
         scrollDirection: Axis.vertical,
           itemBuilder: (context, index) {
-            return Container(child: VideoWidget(
-              controller: VideoPlayerController.asset('assets/sss.mp4'),
-              autoPlay: true,
-              looping: true,
-            ),
+            return Container(
+              height: MediaQuery.of(context).size.height,
+              width:MediaQuery.of(context).size.width,
+              child: Stack(
+                children: [
+                  VideoWidget(
+                    videoUrl:reelVideos[index].videos
+              ),
+            ])
             );
-          }));
+          }
+          )
+    );
 
   }
 }
 
 class VideoWidget extends StatefulWidget {
-  const VideoWidget({Key? key,
-  required this.controller,
-  required this.autoPlay,
-  required this.looping}) : super(key: key);
-
-  final VideoPlayerController controller;
-  final bool looping;
-  final bool autoPlay;
-
-
-
+  const VideoWidget({Key? key,required this.videoUrl}) : super(key: key);
+  final String videoUrl;
   @override
-  State<VideoWidget> createState() => _VideoWidgetState();
+  State<VideoWidget> createState() => _VideoWidgetState(this.videoUrl);
 }
 
 class _VideoWidgetState extends State<VideoWidget> {
 
-  ChewieController? _chewieController;
+  VideoPlayerController? _videoPlayerController;
+ final String videoUrl;
+  _VideoWidgetState(this.videoUrl);
 
   @override
   void initState() {
     super.initState();
-  _chewieController=ChewieController(
-    videoPlayerController: widget.controller,
-    aspectRatio: 5/8,
-    autoInitialize: true,
-    looping: widget.looping,
-    errorBuilder: (context,errorMessage){
-      return Center(
-        child: Text(errorMessage,style: TextStyle(color: ColorConstants.black),)
-      );
-    }
-  );
-  }
+   _videoPlayerController=VideoPlayerController.asset(videoUrl)
+    ..initialize().then((value){
+      _videoPlayerController!.play();
+      setState(() {
 
-  @override
-  void dispose() {
-    super.dispose();
-    _chewieController!.dispose();
+      });
+    });
   }
 
 
   
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.all(8),
-    child: Chewie(
-      controller: _chewieController!,
-    ));
+    return VideoPlayer(_videoPlayerController!);
+
+  }
+}
+
+
+class PostContent extends StatelessWidget {
+  const PostContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 100,
+        ),
+        Expanded(child: Row(
+          children: [
+            Expanded(
+                child: Container(
+
+            ))
+          ],
+        ))
+      ],
+    );
   }
 }

255cc1eca08b2c73f2e2700a7ace0220a9255652
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-04-15 01:16:27 +0300

    Completed part of Reels Videos

--- lib/model/reels_videos.dart
+++ lib/model/reels_videos.dart
@@ -8,5 +8,8 @@ class ReelsVideos{
 
 List<ReelsVideos> reelVideos=[
   ReelsVideos(videos: 'assets/video.mp4'),
+  ReelsVideos(videos: 'assets/video_1.mp4'),
+  ReelsVideos(videos: 'assets/video_2.mp4'),
+  ReelsVideos(videos: 'assets/video_3.mp4'),
   ReelsVideos(videos: 'assets/road.mp4'),
 ];
